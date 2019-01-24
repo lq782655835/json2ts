475eb56ce8a446263aa69dc6a3e9a1d711d11211
springleo <782655835@qq.com>
2019-01-24 16:54:56 +0800

    fixbug

--- src/entry.js
+++ src/entry.js
@@ -8,7 +8,7 @@ const chalkError = chalk.red
 const chalkSuccess = chalk.green
 
 const writeFile = async (target, content) => {
-    let outPath = path.resolve(__dirname, target + '/test.ts')
+    let outPath = path.resolve(target + '/test.ts')
     await fs.outputFile(outPath, content)
     console.log(chalkSuccess(`ts interface success write。write path: ${outPath}`))
 }
